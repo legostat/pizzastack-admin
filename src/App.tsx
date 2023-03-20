26d1c2122b53c01550d95012d830b7f59b7815f2
Oleg Kolisnyk <k4loki@gmail.com>
2023-03-20 15:57:27 +0200

    add menu list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
+import { useState, useEffect } from "react";
+import buildHasuraProvider from "ra-data-hasura";
+import {
+  Admin,
+  DataProvider,
+  /* ListGuesser, */
+  Loading,
+  Resource,
+} from "react-admin";
+import { CssBaseline } from "@mui/material";
+import { Fragment } from "react";
+import { MenuList } from "./modules/components/menu-list/menu-list.component";
 
 export const App = () => {
-  return (
-    <div className="App">
-      ADMIN PANEL
-    </div>
+  const [dataProvider, setDataProvider] = useState<DataProvider<string> | null>(
+    null
   );
-}
+
+  useEffect(() => {
+    const buildDataProvider = async () => {
+      const dp = await buildHasuraProvider({
+        clientOptions: { uri: "http://localhost:8080/v1/graphql" },
+      });
+      setDataProvider(() => dp);
+    };
+    buildDataProvider();
+  }, []);
+
+  if (!dataProvider) {
+    return <Loading />;
+  }
+
+  return (
+    <Fragment>
+      <CssBaseline />
+      <Admin dataProvider={dataProvider}>
+        <Resource name="menu" list={MenuList} />
+      </Admin>
+    </Fragment>
+  );
+};
